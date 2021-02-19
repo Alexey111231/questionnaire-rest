29d0b30c937a6432ffae7d86d939575a1e8b16c4
Alexey <atestat9@mail.ru>
2021-02-19 19:50:46 +0300

    init

---
+++ src/main/java/ru/vk/sladkiipirojok/questionnairerest/QuestionnaireRestApplication.java
@@ -0,0 +1,13 @@
+package ru.vk.sladkiipirojok.questionnairerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireRestApplication.class, args);
+	}
+
+}
