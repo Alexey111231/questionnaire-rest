3b1ac9e95bc6e458d0681d02137a335fabe1606b
Alexey <atestat9@mail.ru>
2021-02-22 16:13:19 +0300

    config mapper

--- src/main/java/ru/vk/sladkiipirojok/questionnairerest/QuestionnaireRestApplication.java
+++ src/main/java/ru/vk/sladkiipirojok/questionnairerest/QuestionnaireRestApplication.java
@@ -1,19 +1,12 @@
 package ru.vk.sladkiipirojok.questionnairerest;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuestionnaireRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuestionnaireRestApplication.class, args);
-    }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
     }
 }
